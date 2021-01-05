9da09b07596be486e3acaafa5e590ff601623ad0
AnnaLaptop <annakudra59@gmail.com>
2021-01-05 13:08:32 +0700

    js popup, chapter 20

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     # переменные для бд
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # настройки почтового сервера
