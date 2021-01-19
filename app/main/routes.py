17894c8a9b8ed5dff25cb0f2156d64ec4cb96928
AnnaLaptop <annakudra59@gmail.com>
2021-01-20 00:09:50 +0700

    user notifications, chapter 21

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,14 @@
 from app import  db
 from flask import render_template, flash, redirect, url_for, request, g, jsonify, current_app
-from app.main.forms import  EditProfileForm, PostForm, EmptyForm, SearchForm
+from app.main.forms import  EditProfileForm, PostForm, EmptyForm, SearchForm, MessageForm
 from app.translate import translate, detect_lang
 from flask_login import current_user, login_required
-from app.models import User, Post
+from app.models import User, Post, Message, Notification
 from datetime import datetime
 from flask_babel import _, get_locale
 from app.main import bp
+import time
+from threading import Thread
 
 
 # запись последнего времени активности пользователя
@@ -167,6 +169,63 @@ def translate_text():
     return jsonify(translate(request.form['text'],
                              request.form['source_lang'],
                              request.form['dest_lang']))
+
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(msg)
+        user.add_notification('unread_message_count', user.new_messages())
+        db.session.commit()
+        flash(_('Ваше сообщение было отправлено пользователю {}'.format(recipient)))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html', title=_('Отправить сообщение'), form=form, recipient=recipient)
+
+
+# def set_last_message_read_time():
+#     time.sleep(1)
+#     current_user.last_message_read_time = datetime.utcnow()
+#     db.session.commit()
+
+
+@bp.route('/messages')
+@login_required
+def messages():
+    # TODO сделать "прочтение и индикацию новых сообщений"
+    current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    # messages = Message.query.order_by(Message.timestamp.desc()).paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+    messages = current_user.messages_received.order_by(Message.timestamp.desc()).paginate(
+        page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None
+    return render_template('messages.html', title=_('Сообщения'), messages=messages.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/notifications')
+@login_required
+def notifications():
+    since = request.args.get('since', 0.0, type=float)
+    notifications = current_user.notifications.filter(Notification.timestamp >= since).order_by(
+        Notification.timestamp.asc())
+    print([{
+        'user': n.user_id,
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
+    return jsonify([{
+        'name': n.name,
+        'data': n.get_data(),
+        'timestamp': n.timestamp
+    } for n in notifications])
 
 
 @bp.route('/search')
